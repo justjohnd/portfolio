ad65a225abf6c7728874816953eed6f752f2949c
johnd <justjohnd@gmail.com>
2022-03-11 10:09:06 +0900

    Create navbar and hamburger

--- src/Modules/Navbar.jsx
+++ src/Modules/Navbar.jsx
@@ -1,5 +1,33 @@
 const Navbar = () => {
-  return <header className="header"></header>;
+  return (
+    <header className="navbar">
+      <a className="navbar-logo" href="#">
+        john denlinger
+      </a>
+      <input type="checkbox" class="toggle" id="nav-toggle" />
+      <label for="nav-toggle" id="nav-toggle-label">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </label>
+      <nav>
+        <ul className="navbar-nav">
+          <li className="navbar-nav-item">
+            <a href="#">about</a>
+          </li>
+          <li className="navbar-nav-item">
+            <a href="#">work</a>
+          </li>
+          <li className="navbar-nav-item">
+            <a href="#">resume</a>
+          </li>
+          <li className="navbar-nav-item">
+            <a href="#">contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;

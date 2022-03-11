fead591ad474d37de09ba3f21b027bc84961c9aa
johnd <justjohnd@gmail.com>
2022-03-11 12:53:10 +0900

    Add pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Modules/Navbar";
+import Home from "./Pages/Work";
+import Work from "./Pages/Work";
+import About from "./Pages/About";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Home to="/" />} />
+      </Routes>
     </div>
   );
 }
--- src/Modules/Navbar.jsx
+++ src/Modules/Navbar.jsx
@@ -1,28 +1,48 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
+  const [checked, setChecked] = useState(false);
+  console.log(checked);
+
   return (
     <header className="navbar">
       <a className="navbar-logo" href="#">
         john denlinger
       </a>
-      <input type="checkbox" class="toggle" id="nav-toggle" />
-      <label for="nav-toggle" id="nav-toggle-label">
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
+      <input
+        type="checkbox"
+        defaultChecked={checked}
+        onChange={() => setChecked(!checked)}
+        className="toggle"
+        id="nav-toggle"
+      />
+      <label htmlFor="nav-toggle" id="nav-toggle-label">
+        <div className={checked ? "bar barChecked" : "bar"}></div>
+        <div className={checked ? "bar barChecked" : "bar"}></div>
+        <div className={checked ? "bar barChecked" : "bar"}></div>
       </label>
-      <nav>
+      <nav className={checked ? "nav-wrapper navChecked" : "nav-wrapper"}>
         <ul className="navbar-nav">
           <li className="navbar-nav-item">
-            <a href="#">about</a>
+            <Link onClick={() => setChecked(!checked)} to="/about">
+              about
+            </Link>
           </li>
           <li className="navbar-nav-item">
-            <a href="#">work</a>
+            <Link onClick={() => setChecked(!checked)} to="/work">
+              work
+            </Link>
           </li>
           <li className="navbar-nav-item">
-            <a href="#">resume</a>
+            <a onClick={() => setChecked(!checked)} href="#">
+              resume
+            </a>
           </li>
           <li className="navbar-nav-item">
-            <a href="#">contact</a>
+            <Link onClick={() => setChecked(!checked)} to="home/#contact">
+              contact
+            </Link>
           </li>
         </ul>
       </nav>

29e04ed5ae94a80788d4f369c2574b8b9abc7c69
johnd <justjohnd@gmail.com>
2022-03-24 10:57:01 +0900

    Move styled components outside functional components

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,43 @@
 import styled from "styled-components";
 
+const CardStyle = styled.button`
+  position: relative;
+  width: 100%;
+  max-height: 15rem;
+  padding: 0.5rem;
+  margin: 1rem 0;
+  background: ${(props) => props.cardBackground};
+  border-radius: 8px;
+  display: flex;
+  align-items: flex-end;
+  cursor: pointer;
+  border-color: none;
+  border: 0px;
+  transition: transform 300ms ease-in-out, box-shadow 400ms ease,
+    background 100ms ease;
+  &:hover {
+    box-shadow: 0 1rem 1rem rgba(0, 0, 0, 0.3);
+    transform: translateY(-0.5rem);
+  }
+  @media (min-width: 576px) {
+    margin: 1rem 2rem;
+  }
+  @media (min-width: 768px) {
+    width: 32%;
+    margin: 0;
+  } ;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90%;
+  object-fit: cover;
+  border-radius: 100% 0 0 0;
+  box-sizing: border-box;
+  object-position: ${(props) => (props.id === 3 ? "35px -10px" : "")};
+  background-color: #000;
+`;
+
 const CardName = styled.div`
   position: absolute;
   top: 0;
@@ -19,53 +57,18 @@ const SmallPrint = styled.div`
 `;
 
 const Card = (props) => {
-  const CardStyle = styled.button`
-    position: relative;
-    width: 100%;
-    max-height: 15rem;
-    padding: 0.5rem;
-    margin: 1rem 0;
-    background: ${props.project.cardBackground};
-    border-radius: 8px;
-    display: flex;
-    align-items: flex-end;
-    cursor: pointer;
-    border-color: none;
-    border: 0px;
-    transition: transform 300ms ease-in-out, box-shadow 400ms ease,
-      background 100ms ease;
-    &:hover {
-      box-shadow: 0 1rem 1rem rgba(0, 0, 0, 0.3);
-      transform: translateY(-0.5rem);
-    }
-    @media (min-width: 576px) {
-      margin: 1rem 2rem;
-    }
-    @media (min-width: 768px) {
-      width: 32%;
-      margin: 0;
-    } ;
-  `;
-
-  const Image = styled.img`
-    width: 100%;
-    height: 90%;
-    object-fit: cover;
-    border-radius: 100% 0 0 0;
-    box-sizing: border-box;
-    object-position: ${props.project.id === 3 ? "35px -10px" : ""};
-    background-color: #000;
-  `;
-
   const { nickname, shortDescription, cardImage } = props.project;
 
   return (
-    <CardStyle onClick={() => props.handleCard(props.project)}>
+    <CardStyle
+      cardBackground={props.project.cardBackground}
+      onClick={() => props.handleCard(props.project)}
+    >
       <CardName>
         <div>{nickname}</div>
         <SmallPrint>{shortDescription}</SmallPrint>
       </CardName>
-      <Image src={cardImage}></Image>
+      <Image id={props.project.id} src={cardImage}></Image>
     </CardStyle>
   );
 };

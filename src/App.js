8d36b9f60b1a25422a79ccbaa8de776f8624019a
johnd <justjohnd@gmail.com>
2022-03-25 09:56:35 +0900

    Add FadeWrapper utility

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import { CSSTransition } from "react-transition-group";
-import { useState } from "react";
 import Navbar from "./Modules/Navbar";
 import HomePage from "./Pages/HomePage";
 import WorkPage from "./Pages/WorkPage";
 import AboutPage from "./Pages/AboutPage";
+import FadeWrapper from "./Utilities/FadeWrapper";
 
 function App() {
-  const [appearHome, setAppearHome] = useState(true);
-
   return (
     <div>
-      <CSSTransition
-        in={appearHome}
-        appear={true}
-        timeout={1000}
-        classNames="fade"
-      >
-        <Navbar />
-      </CSSTransition>
+      {FadeWrapper(<Navbar />)}
       <div className="page-wrapper">
         <Routes>
           <Route path="/home" element={<HomePage />} />
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,20 +1,16 @@
-import { CSSTransition } from "react-transition-group";
 import WorkSection from "../Modules/WorkSection";
 
 const HomePage = () => {
   return (
     <div>
-      <CSSTransition in={true} appear={true} timeout={1000} classNames="fade">
-        <div className="intro-wrapper">
-          <h1>Hi, I'm John!</h1>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Consequatur ullam dignissimos, expedita vero repudiandae veniam ad
-            ducimus adipisci, provident quia dicta nam voluptatem harum
-            accusamus!
-          </p>
-        </div>
-      </CSSTransition>
+      <div className="intro-wrapper">
+        <h1>Hi, I'm John!</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+          ullam dignissimos, expedita vero repudiandae veniam ad ducimus
+          adipisci, provident quia dicta nam voluptatem harum accusamus!
+        </p>
+      </div>
       <WorkSection />
     </div>
   );

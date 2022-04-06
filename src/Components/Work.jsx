4e5ad0922da4c92afdb8e1c34702c2273f990918
johnd <justjohnd@gmail.com>
2022-04-06 11:50:36 +0900

    Add srcset to all images

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -2,7 +2,17 @@ import { v4 as uuidv4 } from "uuid";
 import IconDisplay from "../Utilities/IconDisplay";
 
 const Work = (props) => {
-  const { skills, name, description, image, url, github } = props.showInfo;
+  const {
+    skills,
+    name,
+    description,
+    imageSm,
+    imageMd,
+    imageLg,
+    alt,
+    url,
+    github,
+  } = props.showInfo;
 
   return (
     <>
@@ -32,7 +42,14 @@ const Work = (props) => {
           </div>
         </div>
         <div className="w-50 d-none d-md-block">
-          <img className="image-mockup" src={image}></img>
+          <img
+            className="image-mockup"
+            srcset={`${imageSm} 768w, 
+             ${imageMd} 2000w, 
+             ${imageLg} 4000w`}
+            src={imageMd}
+            alt={alt}
+          ></img>
         </div>
       </div>
     </>

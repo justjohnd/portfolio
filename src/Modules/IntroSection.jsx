9cd16b04e90e5484e96c2a65f5fbd5784e3c1ba3
johnd <justjohnd@gmail.com>
2022-10-07 13:35:23 +0900

    Refactor margins for mobile

--- src/Modules/IntroSection.jsx
+++ src/Modules/IntroSection.jsx
@@ -3,15 +3,18 @@ const IntroSection = () => {
     <section className="intro-wrapper">
       <div className="intro-text">
         <h1>Hi, I'm John!</h1>
-        <p>
+        <p className="text-1">
           I'm a <span className="bold">front-end-focused developer</span> based out of Japan. I come from a career background in engineering, teaching, and professional writing. I’ve been self-studying programming for about three years, and I love making stuff for the web.
-          <br></br><br></br>
-
+        </p>
+        <div className="intro-img-sm">
+          <img className="intro-img" src="../images/john.PNG"></img>
+        </div>
+        <p className="text-2">
           React is my jelly and WordPress is my jam. That is to say, I’ve worked with a number of languages, libraries, and frameworks and am fairly agnostic about what the tech is, so long I can make delightful online experiences.
         </p>
       </div>
-      <div className="intro-img">
-        <img src="../images/john.PNG"></img>
+      <div className="intro-img-lg">
+        <img className="intro-img" src="../images/john.PNG"></img>
       </div>
     </section>
   );
